107e5f566e99957f44938393351b8f1286dc1f4f
rastereo <kapturhouse@yandex.ru>
2023-07-18 20:51:35 +0300

    feat: create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const { PORT = 3000 } = process.env;
+
+const app = express();
+
+app.listen(PORT, () => {
+  // eslint-disable-next-line no-console
+  console.log(`App listening on port ${PORT}`);
+});
