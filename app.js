487b25b2b4bf90d3bc138a06856852e0935dc247
rastereo <kapturhouse@yandex.ru>
2023-07-18 21:59:41 +0300

    feat: connect mongoose to app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-const express = require('express');
+require('dotenv').config();
 
-const { PORT = 3000 } = process.env;
+const express = require('express');
+const mongoose = require('mongoose');
+
+const { PORT = 3000, MONGODB_LINK } = process.env;
 
 const app = express();
 
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`App listening on port ${PORT}`);
+});
+
+mongoose.connect(MONGODB_LINK, {
+  useNewUrlParser: true,
 });
